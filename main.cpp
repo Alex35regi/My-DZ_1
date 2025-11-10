082506a09afc4b712f1cbaf382fd327a19b62163
Alex35regi <ars.aleksander@gmail.com>
2025-11-10 21:25:59 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<clocale>
+#include<iostream>
+#include <Windows.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+	std::cout << "Александр\n";
+	std::cout << 32 << "\n";
+
+	return EXIT_SUCCESS;
+}
